be3c1b03c48291ea2c69d11597a97e29af663b6a
Saleem <saleemsamaldeen@gmail.com>
2024-01-26 19:07:32 +0100

    env variables fixed

--- tests/utils/globalSetup.ts
+++ tests/utils/globalSetup.ts
@@ -2,13 +2,13 @@ import { FullConfig } from "@playwright/test";
 import dotenv from 'dotenv';
 
 async function globalSetup(config:FullConfig) {
-
-    if(process.env.test) {
-        dotenv.config({
-            path: `../resources/.${process.env.test}`,
-        })
-    }
-    
+    console.log(process.env.ENV);
+    var path = `./resources/.env.${process.env.ENV}`
+    dotenv.config({              
+        path: path,
+        override: true  //override existing value declared in config file
+    })
+    console.log("testdata");
 }
 
 export default globalSetup;
